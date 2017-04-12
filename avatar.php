65e9d51f7928681afd414e745ec52e50489e3c59
artskory <contact@artskory.fr>
2017-04-12 16:19:11 +0200

    Creation de la premiere partie de l'espace membre. Mise en place de la page inscription, connexion déconnexion mot de pase oublié chager de mot de passe et changer d'image de profil

--- avatar.php
+++ avatar.php
@@ -1,3 +1,37 @@
+<?php 
+    session_start();
+    if ( !isset( $_SESSION['id'] ) ) {
+        header( 'Location: connexion.php' );
+    }
+    
+    require 'include/functions.php';
+    
+    $membre = account_informations();
+    
+    if ( isset( $_FILES['avatar'] ) && $_FILES['avatar']['error'] == 0 ) {
+        if ( !in_array( $_FILES['avatar']['type'], ['image/png', 'image/jpeg'] ) ) {
+            $erreur = 'Format incorrect (PNG et JPEG acceptés)';
+        }
+        elseif ( $_FILES['avatar']['size'] > 512000 ) {
+            $erreur = 'Image trop volumineuse (supériaur à 100Ko)';
+        }
+        
+        if ( !isset( $erreur ) ) {
+            $extention = str_replace( 'image/', '', $_FILES['avatar']['type'] );
+            $name = bin2hex( random_bytes( 8 ) ) . '.' . $extention;
+            
+            if ( move_uploaded_file( $_FILES['avatar']['tmp_name'], 'img/' . $name ) ){
+                // mise a jpor de la bdd et suppression ancienne image
+                
+                header( 'Location: avatar.php');
+            }
+            else {
+                $erreur = 'Erreur lors de l\'envoi du fichier';
+            }
+        }
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="fr">
   <head>
@@ -8,25 +42,7 @@
     <link rel="stylesheet" href="css/style.css">
   </head>
   <body>
-    <nav class="navbar navbar-dark bg-success">
-      <div class="container">
-        <a class="navbar-brand" href="index.php">monsite.dev</a>
-        <ul class="nav navbar-nav pull-xs-right text-xs-center">
-          <li class="nav-item">
-            <a class="nav-link" href="compte.php">Compte</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="deconnexion.php">Déconnexion</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="inscription.php">Inscription</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="connexion.php">Connexion</a>
-          </li>
-        </ul>
-      </div>
-    </nav>
+    <?php include 'include/header.php'; ?>
     <div class="container profil">
       <div class="row">
         <div class="col-xl-4 col-xl-offset-4 col-md-6 col-md-offset-3">
@@ -34,6 +50,9 @@
           <div class="center-block">
             <img src="" alt="avatar" class="img-circle img-fluid">
           </div>
+            <?php if ( isset( $erreur ) ) : ?>
+                <div class="alert alert-danger"><?= $erreur ?></div>
+            <?php endif; ?>
           <form action="avatar.php" method="post" enctype="multipart/form-data" class="p-y-3 p-x-2">
             <input type="file" name="avatar" class="form-control-file">
             <input type="submit" class="btn btn-success" value="Changer d'avatar">
--- inscription.php
+++ inscription.php
@@ -1,3 +1,48 @@
+<?php 
+    session_start();
+    if ( isset( $_SESSION['id'] ) ) {
+        header( 'Location: compte.php' );
+    }
+    
+    if (!empty( $_POST ) ) {
+        extract( $_POST );
+        $erreur = [];
+        
+        require_once 'include/functions.php';
+        
+        if ( empty( $email ) ) {
+            $erreur['email'] = 'Adresse e-mail maquante';
+        }
+        elseif ( !filter_var( $email, FILTER_VALIDATE_EMAIL ) ) {
+            $erreur['email'] = 'Adresse e-mail invalide';
+        }
+        elseif ( !mail_free() ) {
+            $erreur['email'] = 'Adresse e-mail déja existante';
+        }
+        
+        if ( empty( $password ) ) {
+            $erreur['password'] = 'Mot de passe maquant';
+        }
+        elseif (strlen( $password ) < 8 ) {
+            $erreur['password'] = 'Le mot de passe doit faire au moins 8 caractères';
+        }
+        
+        if ( empty( $passwordconf ) ) {
+            $erreur['passwordconf'] = 'Confirmation du mot de passe maquante';
+        }
+        elseif ( $passwordconf != $password ) {
+            $erreur['passwordconf'] = 'Confirmation du mot de passe différente';
+        }
+        
+        if ( !$erreur ) {
+            // insertion du nouveau membre en bdd
+            
+            unset( $email);
+            
+            $validation = 'Inscription réussie !';
+        }
+    }
+?>
 <!DOCTYPE html>
 <html lang="fr">
   <head>
@@ -8,31 +53,25 @@
     <link rel="stylesheet" href="css/style.css">
   </head>
   <body>
-    <nav class="navbar navbar-dark bg-success">
-      <div class="container">
-        <a class="navbar-brand" href="index.php">monsite.dev</a>
-        <ul class="nav navbar-nav pull-xs-right text-xs-center">
-          <li class="nav-item">
-            <a class="nav-link" href="compte.php">Compte</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="deconnexion.php">Déconnexion</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="inscription.php">Inscription</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="connexion.php">Connexion</a>
-          </li>
-        </ul>
-      </div>
-    </nav>
+    <?php include 'include/header.php'; ?>
     <div class="container">
       <h1 class="text-xs-center">Inscription</h1>
       <div class="row">
         <div class="col-xl-4 col-xl-offset-4 col-md-6 col-md-offset-3">
+            <?php if ( isset( $erreur['email'] ) ) : ?>
+                <div class="alert alert-danger"><?= $erreur['email'] ?></div>
+            <?php endif; ?>
+            <?php if ( isset( $erreur['password'] ) ) : ?>
+                <div class="alert alert-danger"><?= $erreur['password'] ?></div>
+            <?php endif; ?>
+            <?php if ( isset( $erreur['passwordconf'] ) ) : ?>
+                <div class="alert alert-danger"><?= $erreur['passwordconf'] ?></div>
+            <?php endif; ?>
+            <?php if ( isset( $validation ) ) : ?>
+                <div class="alert alert-success"><?= $validation ?></div>
+            <?php endif; ?>
           <form action="inscription.php" method="post" class="p-y-3 p-x-2" novalidate>
-            <input type="email" name="email" class="form-control" placeholder="Adresse e-mail">
+              <input type="email" name="email" class="form-control" placeholder="Adresse e-mail" value="<?php if ( isset( $email ) ) echo $email; ?>">
             <input type="password" name="password" class="form-control" placeholder="Mot de passe">
             <input type="password" name="passwordconf" class="form-control" placeholder="Confirmez le mot de passe">
             <input type="submit" class="btn btn-success" value="Inscription">
